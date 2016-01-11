eb9f8cfc7ea99ee6983dab95628ccd64346319c8
dada <bg5sbk@gmail.com>
2016-01-11 14:14:27 +0800

    update test

--- sync_test.go
+++ sync_test.go
@@ -21,3 +21,63 @@ func Test_SyncPool_AllocLarge(t *testing.T) {
 	utest.EqualNow(t, cap(mem), 2048)
 	pool.Free(mem)
 }
+
+func Benchmark_SyncPool_AllocAndFree_128(b *testing.B) {
+	pool := NewSyncPool(128, 1024, 2)
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			pool.Free(pool.Alloc(128))
+		}
+	})
+}
+
+func Benchmark_SyncPool_AllocAndFree_256(b *testing.B) {
+	pool := NewSyncPool(128, 1024, 2)
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			pool.Free(pool.Alloc(256))
+		}
+	})
+}
+
+func Benchmark_SyncPool_AllocAndFree_512(b *testing.B) {
+	pool := NewSyncPool(128, 1024, 2)
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			pool.Free(pool.Alloc(512))
+		}
+	})
+}
+
+func Benchmark_Make_128(b *testing.B) {
+	b.RunParallel(func(pb *testing.PB) {
+		var x []byte
+		for pb.Next() {
+			x = make([]byte, 128)
+		}
+		x = x[:0]
+	})
+}
+
+func Benchmark_Make_256(b *testing.B) {
+	b.RunParallel(func(pb *testing.PB) {
+		var x []byte
+		for pb.Next() {
+			x = make([]byte, 256)
+		}
+		x = x[:0]
+	})
+}
+
+func Benchmark_Make_512(b *testing.B) {
+	b.RunParallel(func(pb *testing.PB) {
+		var x []byte
+		for pb.Next() {
+			x = make([]byte, 512)
+		}
+		x = x[:0]
+	})
+}
