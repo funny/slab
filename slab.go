7c6981bd0e511e324ed89aa209b623a8ef6f0486
idada <bg5sbk@gmail.com>
2016-10-25 20:20:55 +0800

    增加NoPool

--- slab.go
+++ slab.go
@@ -5,6 +5,15 @@ type Pool interface {
 	Free([]byte)
 }
 
-var _ = Pool((*ChanPool)(nil))
-var _ = Pool((*SyncPool)(nil))
-var _ = Pool((*AtomPool)(nil))
+type NoPool struct{}
+
+func (p *NoPool) Alloc(size int) []byte {
+	return make([]byte, size)
+}
+
+func (p *NoPool) Free(_ []byte) {}
+
+var _ Pool = (*NoPool)(nil)
+var _ Pool = (*ChanPool)(nil)
+var _ Pool = (*SyncPool)(nil)
+var _ Pool = (*AtomPool)(nil)
